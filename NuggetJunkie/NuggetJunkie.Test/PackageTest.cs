9bfbd1ba7a64ce417c61c2599cfbc16a7e108ba0
xafero <georg.berendt@outlook.com>
2017-04-03 21:33:10 +0200

    Can be read

--- NuggetJunkie/NuggetJunkie.Test/PackageTest.cs
+++ NuggetJunkie/NuggetJunkie.Test/PackageTest.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json;
-using NuggetJunkie.Core;
+﻿using NuggetJunkie.Core;
 using NUnit.Framework;
 using System;
 using System.Linq;
@@ -20,15 +19,11 @@ namespace NuggetJunkie.Test
             var file1 = files.First();
             var model1 = PackageModel.LoadFile(file1);
             AreEqual("{'Id':'CommandLineParser','Version':'2.1.1-beta','TargetFramework':'net451'}",
-                ToJson(model1.Entries.First()));
+                model1.Entries.First().ToJson());
             var file2 = files.Last();
             var model2 = PackageModel.LoadFile(file2);
-            AreEqual(2, model2.Entries.Count);
             AreEqual("{'Id':'Newtonsoft.Json','Version':'10.0.2','TargetFramework':'net451'}",
-                ToJson(model2.Entries.First()));
+                model2.Entries.First().ToJson());
         }
-
-        string ToJson(object obj)
-            => JsonConvert.SerializeObject(obj).Replace('"', '\'');
     }
 }
---
+++ NuggetJunkie/NuggetJunkie.Test/TestUtils.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace NuggetJunkie.Test
+{
+    public static class TestUtils
+    {
+        public static string ToJson(this object obj)
+             => JsonConvert.SerializeObject(obj).Replace('"', '\'');
+    }
+}
